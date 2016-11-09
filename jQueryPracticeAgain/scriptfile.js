75b754cc1350f345a5829699257e6fce6c3dade4
ESchulstad <eschulstad@gmail.com>
2016-11-09 17:30:29 -0500

    initial commit

---
+++ jQueryPracticeAgain/scriptfile.js
@@ -0,0 +1,22 @@
+﻿//$(document).ready(function () {
+//    if ($("#par").text() == "A paragraph") {
+//        alert($("#myDiv").html());
+//        alert($("#inBox").val());
+//    }
+//});
+
+
+$(document).ready(function () {
+    //$("#button1").click(function () {
+    //    $("p").after("<b>any text </b>");
+    //});
+
+    //$("#button2").click(function () {
+    //    $("ol").append("<li>another movie </li>");
+    //});
+
+    if ($("#par").text() == "This is a paragraph.") {
+        alert($("#myDiv").html());
+        alert($("#inBox").val());
+    }
+});
